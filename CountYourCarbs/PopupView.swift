46bcfd3bd55cb2b68df0e5c12bf49b5b18bb9602
Lailah <cchslailahnabegu@gmail.com>
2022-02-20 16:51:38 -0500

    Popup Screen

---
+++ CountYourCarbs/PopupView.swift
@@ -0,0 +1,46 @@
+//
+//  Popup View.swift
+//  CountYourCarbs
+//
+import SwiftUI
+
+struct PopupView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct PopupView_Previews: PreviewProvider {
+   static var previews: some View {
+        ContentView()
+    }
+}
+
+struct Menu : View {
+    var body: some View {VStack (alignment:.leading, spacing: 15) {
+    Button (action: {
+    }) {
+        HStack(spacing: 12) {
+            Image("Rectangle 30").renderingMode(.original).resizable().frame(width: 38, height: 5)
+        }
+    }
+    Button (action: {
+    }) {
+        HStack(spacing: 12) {
+            Image("g of greenhouse gases").renderingMode(.original).resizable().frame(width: 38, height: 5)
+            
+}
+        Button (action: {
+}) {
+HStack(spacing: 12) {
+    Image("the carbon footprint of your meal is_").renderingMode(.original).resizable().frame(width: 38, height: 5)
+}
+Button (action: {
+}) {
+    HStack(spacing: 12) {
+        Image("this is the equivalent of_").renderingMode(.original).resizable().frame(width: 38, height: 5)
+    }
+}}
+}
+    }}}
