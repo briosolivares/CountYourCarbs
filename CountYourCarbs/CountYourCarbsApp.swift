a3df46bb410992e89bbf5e67cb85eeb56c01c2c9
Brios Olivares <bolivares8090@gmail.com>
2022-02-20 12:22:18 -0500

    Initial Commit

---
+++ CountYourCarbs/CountYourCarbsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountYourCarbsApp.swift
+//  CountYourCarbs
+//
+//  Created by Brios Olivares on 2/20/22.
+//
+
+import SwiftUI
+
+@main
+struct CountYourCarbsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
